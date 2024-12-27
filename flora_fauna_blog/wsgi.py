744f21a04979da58f5a4e42f98e228eb00be9f7a
RayRivero <rayrivero20@gmail.com>
2024-12-27 00:40:26 -0300

    Initial commit: Django project setup

---
+++ flora_fauna_blog/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for flora_fauna_blog project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flora_fauna_blog.settings')
+
+application = get_wsgi_application()
