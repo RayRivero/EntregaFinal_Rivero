b9054e3c9476652552e65d2572031fdc98b5527f
RayRivero <rayrivero20@gmail.com>
2025-01-19 16:34:35 -0300

    agrego al modelo post para que devuelva el objeto creado

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
@@ -12,6 +13,8 @@ class Post(models.Model):
     
     def __str__(self):
         return self.title
+    def get_absolute_url(self):
+        return reverse('post_detail', kwargs={'pk': self.pk})
     
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
