d9e160d4f84b84132fc791c1571199022d8063c3
RayRivero <rayrivero20@gmail.com>
2025-01-19 20:18:18 -0300

    no muestra imagen, corrijo

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,8 @@ from django.views.generic import RedirectView
 from . import views
 from .views import PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, PostListView
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home_view, name='home'),
@@ -17,3 +19,5 @@ urlpatterns = [
     path('temas/', views.temas_view, name='temas'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
